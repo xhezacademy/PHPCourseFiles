7fe2d5471790c64d8e50488a9a0a102cec679cf1
Arian Xhezairi <arian@xhezairi.com>
2015-05-26 16:29:06 +0200

    Oops.

--- SimpleBlog/app/start.php
+++ SimpleBlog/app/start.php
@@ -1,26 +1,9 @@
 <?php
 
-define('INC_ROOT', dirname(__DIR__));
-
-define('ASSET_ROOT', INC_ROOT . 'public/assets');
-
-define('HTTP_ROOT',
-    'http://' . $_SERVER['HTTP_HOST'] .
-    str_replace(
-        'C:/wamp/www',
-        '',
-        str_replace('\\', '/', INC_ROOT.'/public/')
-    )
-);
-
 /**
  * Vendor Packages Autoload
  */
+require_once 'common.php';
 require_once INC_ROOT . '/vendor/autoload.php';
-
 require_once INC_ROOT . '/app/database.php';
 
-/**
- * Start Session
- */
-session_start();
